911b755d8e3aac6f6957be6e14ef5b6708c23e59
Andrew Yip <andrew.yip22@gmail.com>
2021-03-13 23:34:30 -1000

    modified files and progressed with multiple counters and navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-      count: 0,
-      tags: []
-   };
-
-   renderTags() {
-       if (this.state.tags.length === 0) return <p>There are no tags!</p>
-    
-       return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-   }
-
 
   render() {
+
+    console.log('props' , this.props);
+
     return (
       <div>
-        {this.state.tags.length === 0 && 'Please create a new tag!'}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary tbn-sm">Increment</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
 
+  getBadgeClasses () {
+      let classes = "badge m-2 badge-";
+      classes += this.props.counter.value === 0 ? "warning" : "primary";
+      return classes;
+  }
+
+  formatCount () {
+      const {value} = this.props.counter;
+      return value === 0 ? 'Zero' : value;
+  }
+
 }
+
 
 export default Counter;
